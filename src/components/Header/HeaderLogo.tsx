5141d390b4381fbe64d37000602bdcf242f07911
Ilya Semikashev <tebesski@gmail.com>
2024-05-16 00:50:05 +0300

    fixed

--- src/components/Header/HeaderLogo.tsx
+++ src/components/Header/HeaderLogo.tsx
@@ -1,13 +1,11 @@
+import logo from "../../../assets/logo.png"
+import gecko from "../../../assets/gecko.png"
+
 export default function HeaderLogo() {
    return (
       <div className="flex text-green-main justify-between px-6 mt-4">
          <div className="flex gap-x-4 items-center flex-1">
-            <img
-               src="../../../assets/logo.png"
-               alt="logo"
-               width={80}
-               height={80}
-            />
+            <img src={logo} alt="logo" width={80} height={80} />
 
             <span className="flex flex-col gap-y-1">
                <h1 className="font-semibold text-xl">ECO-GECKO CREATION LLC</h1>
@@ -16,7 +14,7 @@ export default function HeaderLogo() {
          </div>
 
          <img
-            src="../../../assets/gecko.png"
+            src={gecko}
             alt="gecko"
             className="object-contain block"
             width={309 / 2.5}
